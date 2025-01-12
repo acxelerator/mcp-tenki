ffdabff709138e17053ef9c06d6b804a87f377ae
gsy0911 <yoshiki0911@gmail.com>
2025-01-12 12:03:05 +0900

    remove files

--- mcp_tenki/location.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Location(BaseModel):
-    prefecture: str = Field(alias="pref")
-    city: str
-    id_: str = Field(alias="id")
--- mcp_tenki/main.py
+++ mcp_tenki/main.py
@@ -1,5 +1,4 @@
-from .weather import WeatherForecast
-from .location import Location
+from pydantic import BaseModel, Field
 
 import httpx
 from typing import Any
@@ -153,6 +152,62 @@ location_data = [
     {"pref": "沖縄県", "city": "石垣島", "id": "474010"},
     {"pref": "沖縄県", "city": "与那国島", "id": "474020"},
 ]
+
+
+class Location(BaseModel):
+    prefecture: str = Field(alias="pref")
+    city: str
+    id_: str = Field(alias="id")
+
+
+class Temperature(BaseModel):
+    celsius: str | None
+    fahrenheit: str | None
+
+
+class TemperatureRange(BaseModel):
+    min: Temperature
+    max: Temperature
+
+
+class WeatherDetail(BaseModel):
+    weather: str
+    wind: str
+    wave: str
+
+
+class WeatherImage(BaseModel):
+    title: str
+    url: str
+    width: int
+    height: int
+
+
+class ChanceOfRain(BaseModel):
+    T00_06: str
+    T06_12: str
+    T12_18: str
+    T18_24: str
+
+
+class WeatherForecast(BaseModel):
+    date: str
+    date_label: str = Field(alias="dateLabel")
+    telop: str
+    detail: WeatherDetail = Field(alias="detail")
+    temperature: TemperatureRange
+    chance_of_rain: ChanceOfRain = Field(alias="chanceOfRain")
+
+    def format_forecast(self) -> str:
+        return f"""
+            Daet: {self.date}
+            TemperatureMin: {self.temperature.min.celsius}度
+            TemperatureMax: {self.temperature.max.celsius}度
+            ChanceOfRain(00_06): {self.chance_of_rain.T00_06}
+            ChanceOfRain(06_12): {self.chance_of_rain.T06_12}
+            ChanceOfRain(12_18): {self.chance_of_rain.T12_18}
+            ChanceOfRain(18_24): {self.chance_of_rain.T18_24}
+            """
 
 
 async def make_nws_request(url: str) -> dict[str, Any] | None:
